c820773255126b3ac0b27b443a882195d1805684
Appile1 <ahmadsiddiquej3@gmail.com>
2024-01-24 16:18:39 +0500

    improvements

--- watchlist.js
+++ watchlist.js
@@ -12,9 +12,14 @@ function updateWatchlistUI() {
   } else {
     movies.forEach((movie) => {
       watchlistHTML += `
-          <div class="watchlist-item">
-            <h3>${movie.Title}</h3>
-          </div>
+      <div class="movie">
+      <img class="movie-poster" src="${movie.Poster}" />
+      <div>
+        <h3>${movie.Title}</h3>
+        <p>${movie.Year}</p>
+        <button class="Watchlist-btn" data-imdbid="${movie.imdbID}">Add to Watchlist</button>
+      </div>
+    </div>
         `;
     });
 
